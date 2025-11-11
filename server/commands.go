6565b207fc89e3b91e9396c49c66657b56a7489b
GiuseppeSorgentone <giuseppe.sorgentone.mail@gmail.com>
2025-11-11 01:30:42 +0100

    initial commit

--- server/commands.go
+++ server/commands.go
@@ -3,7 +3,7 @@ package main
 func getConstantCommands() map[string][]string {
 	return map[string][]string{
 		"GET":  []string{"GET", "Get", "get"},
-		"PUT":  []string{"PUT", "Put", "put"},
+		"SET":  []string{"SET", "Set", "set"},
 		"DEL":  []string{"DEL", "Del", "del"},
 		"ESC":  []string{"ESC", "Esc", "esc"},
 		"PING": []string{"PING", "Ping", "ping"},
